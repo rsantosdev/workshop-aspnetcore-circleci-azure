27e0183d2d84ac600ec94a18dc7aae973083dba8
Weverton Gomes <weverton@gmail.com>
2016-11-30 23:11:36 -0200

    Teste quebrado

--- test/UnitTest/Class1.cs
+++ test/UnitTest/Class1.cs
@@ -12,11 +12,11 @@ namespace UnitTest
             Assert.Equal(4, Add(2, 2));
         }
 
-        // [Fact]
-        // public void FailingTest()
-        // {
-        //     Assert.Equal(5, Add(2, 2));
-        // }
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
 
         int Add(int x, int y)
         {
